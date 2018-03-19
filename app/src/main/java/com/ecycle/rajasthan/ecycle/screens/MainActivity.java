e766707dadd9c17dd3b5d1ec9896f4af55a59bfc
Kishan <kishanjvaghela@gmail.com>
2018-03-20 03:58:44 +0530

    added empty profile and scan fragment

--- app/src/main/java/com/ecycle/rajasthan/ecycle/screens/MainActivity.java
+++ app/src/main/java/com/ecycle/rajasthan/ecycle/screens/MainActivity.java
@@ -1,15 +1,49 @@
 package com.ecycle.rajasthan.ecycle.screens;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.ecycle.rajasthan.ecycle.R;
+import com.ecycle.rajasthan.ecycle.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        mBinding.bottomNavigation.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_scan_cycle:
+                setFragment(new ScanFragment());
+                return true;
+            case R.id.action_find_cycle:
+                setFragment(new HomeFragment());
+                return true;
+            case R.id.action_profile:
+                setFragment(new ProfileFragment());
+                return true;
+        }
+        return true;
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.contentLayout,fragment);
+        transaction.commit();
     }
 }
