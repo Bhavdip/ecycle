b6915f175d956f6cae06a877134bbfaf309b42e6
bhavdip <bhavdip.pathar@gmail.com>
2018-03-20 05:00:56 +0530

    added support map fragment inside home fragment.

--- app/src/main/java/com/ecycle/rajasthan/ecycle/screens/HomeFragment.java
+++ app/src/main/java/com/ecycle/rajasthan/ecycle/screens/HomeFragment.java
@@ -11,6 +11,12 @@ import android.view.ViewGroup;
 
 import com.ecycle.rajasthan.ecycle.R;
 import com.ecycle.rajasthan.ecycle.databinding.FragmentHomeBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.kishan.askpermission.AskPermission;
 import com.kishan.askpermission.ErrorCallback;
 import com.kishan.askpermission.PermissionCallback;
@@ -20,12 +26,15 @@ import com.kishan.askpermission.PermissionInterface;
  * Created by bhavdip on 3/20/18.
  */
 
-public class HomeFragment extends Fragment implements PermissionCallback, ErrorCallback {
+public class HomeFragment extends Fragment implements PermissionCallback, ErrorCallback, OnMapReadyCallback {
 
     private static final int REQUEST_PERMISSIONS = 20;
-
-
     private FragmentHomeBinding mHomeBinding;
+    private GoogleMap mGoogleMap;
+
+    public static HomeFragment getInstance() {
+        return new HomeFragment();
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,10 +51,10 @@ public class HomeFragment extends Fragment implements PermissionCallback, ErrorC
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        askForPermision();
+        askForPermission();
     }
 
-    private void askForPermision() {
+    private void askForPermission() {
         new AskPermission.Builder(this)
                 .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
                 .setCallback(this)
@@ -57,7 +66,7 @@ public class HomeFragment extends Fragment implements PermissionCallback, ErrorC
 
     @Override
     public void onPermissionsGranted(int requestCode) {
-
+        initGMaps();
     }
 
     @Override
@@ -73,5 +82,21 @@ public class HomeFragment extends Fragment implements PermissionCallback, ErrorC
     @Override
     public void onShowSettings(PermissionInterface permissionInterface, int requestCode) {
 
+    }
+
+    private void initGMaps() {
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mGoogleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
--- app/src/main/java/com/ecycle/rajasthan/ecycle/screens/MainActivity.java
+++ app/src/main/java/com/ecycle/rajasthan/ecycle/screens/MainActivity.java
@@ -1,15 +1,29 @@
 package com.ecycle.rajasthan.ecycle.screens;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import com.ecycle.rajasthan.ecycle.R;
+import com.ecycle.rajasthan.ecycle.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        loadHomeFragment();
+    }
+
+    private void loadHomeFragment() {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.container, HomeFragment.getInstance());
+        fragmentTransaction.commit();
+
     }
 }
