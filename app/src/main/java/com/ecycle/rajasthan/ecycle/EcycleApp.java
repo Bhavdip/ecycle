3922f2d02ef23f14eb0a86cf6d468a4d35c6a384
navit gupta <navitgupta@gmail.com>
2018-03-19 19:32:07 +0530

    initial commit

---
+++ app/src/main/java/com/ecycle/rajasthan/ecycle/EcycleApp.java
@@ -0,0 +1,10 @@
+package com.ecycle.rajasthan.ecycle;
+
+import android.app.Application;
+
+/**
+ * Created by Kishan Vaghela on 3/19/2018.
+ */
+
+public class EcycleApp extends Application {
+}
---
+++ app/src/main/java/com/ecycle/rajasthan/ecycle/screens/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ecycle.rajasthan.ecycle.screens;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ecycle.rajasthan.ecycle.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
